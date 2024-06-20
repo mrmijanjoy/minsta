dc04a9b2786b1966715df5125e04976f74e1b2dc
Mijanur Rahman Joy <mrmijanjoy@gmail.com>
2024-06-20 20:11:11 +0600

    Add files via upload

---
+++ pages/register.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return
+  }
+}
+
+class RegisterPageState extends State<RegisterPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(
+        color: Colors.red,
+      ), 
+    );
+  }
+}
